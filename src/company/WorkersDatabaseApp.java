5f69864aa9a394d3616e51d724fc2e16d57a8f8f
Andrzej <andrzej.polednik@gmail.com>
2018-05-23 12:19:46 +0200

    Initial add to repository

---
+++ src/company/WorkersDatabaseApp.java
@@ -0,0 +1,36 @@
+package company;
+
+import java.util.Scanner;
+
+public class WorkersDatabaseApp {
+
+	public static void main(String[] args) {
+		
+		
+		Workers wrk = new Workers();
+		System.out.println(wrk.info());
+		
+		
+
+		/*
+		
+		System.out.println("Enter how many workers to add");
+		Scanner in = new Scanner(System.in);
+		int numOfWorkers = in.nextInt();
+		Workers[] workers = new Workers[numOfWorkers];
+		
+		
+		
+		for(int n=0; n <numOfWorkers; n++) {
+			workers[n] = new Workers();
+			Workers wrk = new Workers();
+		}
+		
+		for(int n=0; n<numOfWorkers; n++) {
+			System.out.println(workers[n].info());
+		}
+		
+		*/
+	}
+		
+}
---
+++ src/company/WrongCourseSelection.java
@@ -0,0 +1,18 @@
+package company;
+
+public class WrongCourseSelection extends Exception {
+	
+int courseNumber;
+	
+	public WrongCourseSelection(int courseNumber) {
+		this.courseNumber = courseNumber;
+		
+		
+	}
+	public String toString() {
+		return "Error: Choose the right course";
+	}
+	
+
+}
+
