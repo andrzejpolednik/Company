01854257f0cfa4e31ac0063f40d140965f1ad1f7
Sean Doe <lax@lax.net.pl>
2018-05-25 00:12:36 +0200

    Refactor, netbeans config

--- src/company/WorkersDatabaseApp.java
+++ src/company/WorkersDatabaseApp.java
@@ -4,23 +4,18 @@ import java.util.Scanner;
 
 public class WorkersDatabaseApp {
 
-	public static void main(String[] args) {
-		
-		
-		System.out.println("Enter how many workers to add");
-		Scanner in = new Scanner(System.in);
-		int numOfWorkers = in.nextInt();
-		Workers[] workers = new Workers[numOfWorkers];
-		
-		
-		
-		for(int n=0; n <numOfWorkers; n++) {
-			workers[n] = new Workers();
-			
-		}
-		for(int n=0; n <numOfWorkers; n++) {
-			System.out.println(workers[n].info());
-		}
-	}
-		
+    public static void main(String[] args) {
+        System.out.println("Enter how many workers to add");
+        Scanner in = new Scanner(System.in);
+        int numOfWorkers = in.nextInt();
+        Workers[] workers = new Workers[numOfWorkers];
+
+        for (int n = 0; n < numOfWorkers; n++) {
+            workers[n] = new Workers();
+        }
+        for (int n = 0; n < numOfWorkers; n++) {
+            System.out.println(workers[n].info());
+        }
+    }
+
 }
