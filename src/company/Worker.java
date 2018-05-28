8ed733d08974e2ad5a9d6cbbb104dde153d0c2c1
andrzejpolednik <39500506+andrzejpolednik@users.noreply.github.com>
2018-05-28 21:02:33 +0200

    Add files via upload

--- src/company/Worker.java
+++ src/company/Worker.java
@@ -9,8 +9,9 @@ public class Worker {
     private String password;
     private String email;
     private int department;
-    private Position position;
-
+    private int course;
+    private int position;
+    
     public void setFirstName(String name) {
         this.firstName = name;
     }
@@ -19,8 +20,16 @@ public class Worker {
         this.lastName = name;
     }
     
+    public void setPosition(int position) {
+    	this.position = position;
+    }
+    
     public void setDepartment(int department) {
         this.department = department;
+    }
+    
+    public void setCourse(int course) {
+    	this.course = course;
     }
 
     public void generateUserCredentials() {
@@ -34,7 +43,9 @@ public class Worker {
         info.put("id", this.id);
         info.put("firstName", this.firstName);
         info.put("lastName", this.lastName);
+        info.put("position", Position.get(this.position));
         info.put("department", Departments.get(this.department));
+        info.put("course", Courses.get(this.course));
         info.put("email", this.email);
         
         return info;
@@ -61,7 +72,6 @@ public class Worker {
     private String setID() {
         int randomNumber = (int) (Math.random() * Math.pow(10, 4));
         String id = Departments.get(department).substring(0, 1) + randomNumber;
-        //firstDepartmentLetter + "" + 
         return id;
     }
 }
